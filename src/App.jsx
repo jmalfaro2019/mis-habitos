f17095ecd08c09022c1e8aa8b29f5839036f0279
alfarito2019 <joalfaroc@unal.edu.co>
2025-11-24 23:52:34 +0100

    acceso a firebase

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ import {
 // --- CONFIGURACIÓN DE FIREBASE ---
 // ¡¡IMPORTANTE!!: Reemplaza esto con TUS credenciales de Firebase Console
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
+  apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
